da63c2da7aa3130119e72135266fe4709f6179a0
rizller <rizller@hotmail.com>
2024-02-20 12:07:38 -0300

    Implementation of API consumption and usege

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import "./style.css";
+
+//https://sujeitoprogramador.com/rn-api/?api=posts
 
 function App() {
+  const [nutri, setNutri] = useState([]);
+
+
+//sempre quando carregar a página, chama a função useEffect
+  useEffect(()=>{
+    function loadApi(){
+      let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+      fetch(url) //faz uma requisição de get nessa URL
+      .then((r) => r.json()) //transforma o retorno em json
+      .then((json) => {
+        setNutri(json); //seta o json no useState Nutri
+      })
+    
+    }
+
+    loadApi();
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <strong>React Nutri Project</strong>
       </header>
+      {/* pecorre todo o array nutri */}
+      {nutri.map((item)=>{
+        return (
+          <article key={item.id} className='post'>
+            <strong className='titulo'>{item.titulo}</strong>
+            <span className='categoria'><strong>Categoria:</strong> {item.categoria}</span>
+            <img src={item.capa} alt={item.titulo} />
+            <p className={item.subtitulo}>
+              {item.subtitulo}
+            </p>
+            <a className='botao'>Acessar</a>
+          </article>
+        ) 
+      })}
     </div>
   );
 }
